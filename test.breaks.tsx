9d3d4bf312ba16fecbea2d8d0d1a9db66c719c90
dmi3y <dmitryl@syapse.com>
2017-08-25 17:56:03 -0700

    touchbase

---
+++ test.breaks.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class Test extends React.PureComponent {
+  render () {
+    return <div>
+      <div>
+        Breaks
+      </div>
+    </div>
+  }
+}
---
+++ test.works.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default class Test extends React.PureComponent {
+  render () {
+    return <div><div>
+        Pass
+      </div>
+    </div>
+  }
+}
